c7ae41db8bfa1f16a850f66d8c42a7590ff49601
Sara Samuel <henrysm@umich.edu>
2018-02-19 13:44:17 -0500

    Wrote a couple basic tests to ensure mocha is working

--- test/test.js
+++ test/test.js
@@ -6,3 +6,13 @@ describe('Array', function() {
     });
   });
 });
+
+describe ('Math', function() {
+  it('should test that 3 x 3 = 9', function(){
+	assert.equal(9, 3*3);
+  })
+
+  it('should test if -8 is results of equation', function(){
+  	assert.equal(-8, (3-4)*8);
+  })
+})
