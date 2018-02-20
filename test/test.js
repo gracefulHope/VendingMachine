826f7d0518dddcea6f0c4bdfa558e27778f2fc7e
Sara Samuel <henrysm@umich.edu>
2018-02-20 15:27:06 -0500

    Added placeholders for some tests, beginning to work on adding coins

--- test/test.js
+++ test/test.js
@@ -1,11 +1,4 @@
 var assert = require('assert');
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal(-1, [1,2,3].indexOf(4));
-    });
-  });
-});
 
 describe ('Math', function() {
   it('should test that 3 x 3 = 9', function(){
@@ -15,4 +8,30 @@ describe ('Math', function() {
   it('should test if -8 is results of equation', function(){
   	assert.equal(-8, (3-4)*8);
   })
+})
+
+describe ('Vending Machine', function () {
+  describe ('Accepts Coins', function () {
+    it('should accept a nickel', function(){
+	assert(insertCoin('nickel').equal(true));
+  
+    })
+    it('should accept a dime', function (){
+	assert(insertCoin('dime').equal(true));
+    })
+  //it('should accept a quarter', function (){})
+  //it('should reject a penny', function (){})
+  //it('should reject a Canadian coin', function(){}) 
+  })
+
+  describe('Select Product', function (){
+    //it('should dispense a cola when at least $1.00 is available', function(){})
+    //it('should dispense chips when at least $.50 is available', function(){})
+    //it('should dispense candy when at least $.65 is available', function(){})
+    //it('should not dispense cola if only $.50 is available', function(){})
+    //it('should not dispense chips if only $.25 is available', function(){})
+    //it('should not dispense candy if only $.45 is available', function(){})
+  })
+
+
 })
